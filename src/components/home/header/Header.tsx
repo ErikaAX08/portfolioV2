a2cefebe8e3c508a5ddefa2b97274ee2e7ba8b5c
Erika AX <eamastalxochimitl@gmail.com>
2024-04-13 23:13:18 -0600

    Subtitle updated

--- src/components/home/header/Header.tsx
+++ src/components/home/header/Header.tsx
@@ -6,18 +6,15 @@ function Header() {
   return (
     <header className={styles.header}>
       <p className={styles.subtitle}>
-        <Image
-          src="/icons/terminal-solid.svg"
-          alt="Termial icon"
-          width={32}
-          height={32}
-          className={styles.contactIcon}
-        />
-        Available for new projects
+        <span>fullstack</span>
+        <span>/</span>
+        <span>mobile</span>
+        <span>/</span>
+        <span>web</span>
+        <span>/</span>
+        <span>gamedev</span>
       </p>
-      <h1 className={styles.title}>
-        I BUILD digital EXPERIENCES through code
-      </h1>
+      <h1 className={styles.title}>I BUILD digital EXPERIENCES through code</h1>
       <p className={styles.description}>
         Full stack <strong>web</strong> & <strong>mobile app</strong> developer,
         turning dreams into <strong>digital realities.</strong>
