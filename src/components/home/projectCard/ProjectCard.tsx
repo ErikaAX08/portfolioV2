09fd7fdeec602a300b6b98a984ff83beb347ced0
Erika AX <eamastalxochimitl@gmail.com>
2024-06-03 23:10:22 -0600

    Project card properties added

--- src/components/home/projectCard/ProjectCard.tsx
+++ src/components/home/projectCard/ProjectCard.tsx
@@ -1,34 +1,38 @@
 import Image from "next/image";
 import styles from "./ProjectCard.module.css";
 import Link from "next/link";
+import { FC } from "react";
 
-function ProjectCard() {
+interface ProjectCardProps {
+  name: string;
+  description: string[];
+  link: {
+    name: string;
+    url: string;
+  };
+  images: {
+    src: string;
+    alt: string;
+  }[];
+}
+
+const ProjectCard: FC<ProjectCardProps> = ({
+  name,
+  description,
+  link,
+  images,
+}) => {
   return (
     <article className={styles.project}>
       <div className={styles.projectInfo}>
-        <h2 className={styles.title}>Time tracker</h2>
+        <h2 className={styles.title}>{name}</h2>
         <div className={styles.description}>
-          <p>
-            My most recent full-time role, where I led our design efforts as the
-            Head of Design. I worked on our product, was involved in strategic
-            business decisions, and managed a talented incredible design team as
-            we built the future of financial wellness.
-          </p>
-          <p>
-            I was involved in shipping countless features including Cash Flow,
-            Retirement Calculator, Emergency Fund, Multicurrency Support, Global
-            Advisors, Action Plan, Healthcare Comparison, and more.
-          </p>
-          <p>
-            I was also in charge of creating and managing our design system,
-            Bodega, at scale as we expanded our product offerings and pitched to
-            larger companies. My contributions led to closing a $24.4M round of
-            funding from GGV as well as landing deals with Snap, Workday,
-            Discord, Superhuman, Nerdwallet, and more.
-          </p>
+          {description.map((pharagrap, key) => (
+            <p key={key}>{pharagrap}</p>
+          ))}
         </div>
-        <Link className={styles.linkContainer} href="#" target="_blank">
-          <span className={styles.link}>Download app</span>
+        <Link className={styles.linkContainer} href={link.url} target="_blank">
+          <span className={styles.link}>{link.name}</span>
           <Image
             className={styles.iconLink}
             src="/icons/arrow-solid.svg"
@@ -39,51 +43,19 @@ function ProjectCard() {
         </Link>
       </div>
       <div className={styles.imagenes}>
-        <Image
-          className={styles.image}
-          src="/projects/image.png"
-          alt="Project"
-          width={500}
-          height={500}
-        />
-        <Image
-          className={styles.image}
-          src="/projects/image.png"
-          alt="Project"
-          width={500}
-          height={500}
-        />
-        <Image
-          className={styles.image}
-          src="/projects/image.png"
-          alt="Project"
-          width={500}
-          height={500}
-        />
-        <Image
-          className={styles.image}
-          src="/projects/image.png"
-          alt="Project"
-          width={500}
-          height={500}
-        />
-        <Image
-          className={styles.image}
-          src="/projects/image.png"
-          alt="Project"
-          width={500}
-          height={500}
-        />
-        <Image
-          className={styles.image}
-          src="/projects/image.png"
-          alt="Project"
-          width={500}
-          height={500}
-        />
+        {images.map((image, key) => (
+          <Image
+            key={key}
+            className={styles.image}
+            src={image.src}
+            alt={image.alt}
+            width={500}
+            height={500}
+          />
+        ))}
       </div>
     </article>
   );
-}
+};
 
 export default ProjectCard;
