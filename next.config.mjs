61d69dfbc7aaffeaeda6761ba297a704c38649db
ErikaAX08 <eamastalxochimitl@gmail.com>
2025-04-12 17:28:06 -0600

    Next configuration

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,52 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+    reactStrictMode: true,
+
+    // Image optimization
+    images: {
+        unoptimized: true,
+        domains: [],
+    },
+
+    // Configuring webpack to improve cache management
+    webpack: (config, {dev, isServer}) => {
+        if (dev) {
+            config.cache = false;
+        }
+
+        return config;
+    },
+
+    // Setting headers to avoid caching static files
+    async headers() {
+        return [
+            {
+                source: '/images/:path*',
+                headers: [
+                    {
+                        key: 'Cache-Control',
+                        value: 'no-store, no-cache, must-revalidate, proxy-revalidate',
+                    },
+                    {
+                        key: 'Pragma',
+                        value: 'no-cache',
+                    },
+                    {
+                        key: 'Expires',
+                        value: '0',
+                    },
+                ],
+            },
+        ];
+    },
+
+    eslint: {
+        ignoreDuringBuilds: true,
+    },
+
+    typescript: {
+        ignoreBuildErrors: true,
+    },
+};
 
 export default nextConfig;
