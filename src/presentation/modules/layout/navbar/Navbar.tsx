9aa9ed2176813308fc5ad3cf0ce8daa0bfebb648
ErikaAX08 <eamastalxochimitl@gmail.com>
2025-09-07 19:11:17 -0600

    feat: update header structure

--- src/presentation/modules/layout/navbar/Navbar.tsx
+++ src/presentation/modules/layout/navbar/Navbar.tsx
@@ -5,20 +5,31 @@ import styles from "./Navbar.module.css";
 function Navbar() {
   return (
     <nav className={styles.navbar}>
-      <strong className={styles.title}>
-        <Link className={styles.titleLink} href={"/"}>
-        Erika Amastal
-        </Link>
-      </strong>
       <ul className={styles.menu}>
-        <li className={styles.menuItem}>
-          <CommonLink text="Work" url="/#work" />
-        </li>
         <li className={styles.menuItem}>
           <CommonLink text="About" url="/#about" />
         </li>
         <li className={styles.menuItem}>
+          <CommonLink text="Projects" url="/#projects" />
+        </li>
+        <li className={styles.menuItem}>
           <CommonLink text="Contact" url="/contact" />
+        </li>
+      </ul>
+      <strong className={styles.title}>
+        <Link className={styles.titleLink} href={"/"}>
+          Erika Amastal
+        </Link>
+      </strong>
+      <ul className={styles.menu}>
+        <li className={styles.menuItem}>
+          <CommonLink text="Linkedin" url="/#work" />
+        </li>
+        <li className={styles.menuItem}>
+          <CommonLink text="Github" url="/#about" />
+        </li>
+        <li className={styles.menuItem}>
+          <CommonLink text="Instagram" url="/contact" />
         </li>
       </ul>
     </nav>
